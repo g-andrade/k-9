5bebfd76a68cbe5f17ffe83917447e01385958dd
Vincent Breitmoser <valodim@mugenguild.com>
2016-04-18 16:46:34 +0200

    messageview: add support for encapsulated signed parts (fixes #576)
    Conflicts:
    	k9mail/src/main/java/com/fsck/k9/mailstore/LocalMessageExtractor.java
    	k9mail/src/main/java/com/fsck/k9/mailstore/OpenPgpResultAnnotation.java
    	k9mail/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoHelper.java
    
    Conflicts:
    	k9mail/src/main/java/com/fsck/k9/crypto/MessageDecryptVerifier.java
    	k9mail/src/main/java/com/fsck/k9/mailstore/MessageViewInfoExtractor.java

--- k9mail/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoHelper.java
+++ k9mail/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoHelper.java
@@ -65,14 +65,14 @@ public class MessageCryptoHelper {
 
     private MessageCryptoAnnotations messageAnnotations;
     private Intent userInteractionResultIntent;
+    private LocalMessage currentMessage;
+    private boolean secondPassStarted;
 
 
     public MessageCryptoHelper(Activity activity, Account account, MessageCryptoCallback callback) {
         this.context = activity.getApplicationContext();
         this.callback = callback;
         this.account = account;
-
-        this.messageAnnotations = new MessageCryptoAnnotations();
     }
 
     public void decryptOrVerifyMessagePartsIfNecessary(LocalMessage message) {
@@ -81,13 +81,24 @@ public class MessageCryptoHelper {
             return;
         }
 
-        List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(message);
+        this.messageAnnotations = new MessageCryptoAnnotations();
+        this.currentMessage = message;
+
+        runFirstPass();
+    }
+
+    private void runFirstPass() {
+        List<Part> encryptedParts = MessageDecryptVerifier.findEncryptedParts(currentMessage);
         processFoundEncryptedParts(encryptedParts);
 
-        List<Part> signedParts = MessageDecryptVerifier.findSignedParts(message);
+        decryptOrVerifyNextPart();
+    }
+
+    private void runSecondPass() {
+        List<Part> signedParts = MessageDecryptVerifier.findSignedParts(currentMessage, messageAnnotations);
         processFoundSignedParts(signedParts);
 
-        List<Part> inlineParts = MessageDecryptVerifier.findPgpInlineParts(message);
+        List<Part> inlineParts = MessageDecryptVerifier.findPgpInlineParts(currentMessage);
         addFoundInlinePgpParts(inlineParts);
 
         decryptOrVerifyNextPart();
@@ -137,7 +148,7 @@ public class MessageCryptoHelper {
 
     private void decryptOrVerifyNextPart() {
         if (partsToDecryptOrVerify.isEmpty()) {
-            returnResultToFragment();
+            runSecondPassOrReturnResultToFragment();
             return;
         }
 
@@ -458,6 +469,17 @@ public class MessageCryptoHelper {
         onCryptoFinished();
     }
 
+    private void propagateEncapsulatedSignedPart(CryptoResultAnnotation resultAnnotation, Part part) {
+        Part encapsulatingPart = messageAnnotations.findKeyForAnnotationWithReplacementPart(part);
+        CryptoResultAnnotation encapsulatingPartAnnotation = messageAnnotations.get(encapsulatingPart);
+
+        if (encapsulatingPart != null && resultAnnotation.hasSignatureResult()) {
+            CryptoResultAnnotation replacementAnnotation =
+                    encapsulatingPartAnnotation.withEncapsulatedResult(resultAnnotation);
+            messageAnnotations.put(encapsulatingPart, replacementAnnotation);
+        }
+    }
+
     private void onCryptoFailed(OpenPgpError error) {
         CryptoResultAnnotation errorPart = CryptoResultAnnotation.createOpenPgpErrorAnnotation(error);
         addCryptoResultAnnotationToMessage(errorPart);
@@ -467,11 +489,22 @@ public class MessageCryptoHelper {
     private void addCryptoResultAnnotationToMessage(CryptoResultAnnotation resultAnnotation) {
         Part part = currentCryptoPart.part;
         messageAnnotations.put(part, resultAnnotation);
+
+        propagateEncapsulatedSignedPart(resultAnnotation, part);
     }
 
     private void onCryptoFinished() {
         partsToDecryptOrVerify.removeFirst();
         decryptOrVerifyNextPart();
+    }
+
+    private void runSecondPassOrReturnResultToFragment() {
+        if (secondPassStarted) {
+            callback.onCryptoOperationsFinished(messageAnnotations);
+            return;
+        }
+        secondPassStarted = true;
+        runSecondPass();
     }
 
     private void returnResultToFragment() {
