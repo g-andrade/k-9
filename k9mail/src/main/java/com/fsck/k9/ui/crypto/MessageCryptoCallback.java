7510df4fc6de0f9782e634ddc6739b0f9a1d9bc6
Vincent Breitmoser <valodim@mugenguild.com>
2016-05-09 16:28:38 +0200

    messageview: add logic for display of crypto progress

--- k9mail/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoCallback.java
+++ k9mail/src/main/java/com/fsck/k9/ui/crypto/MessageCryptoCallback.java
@@ -6,6 +6,7 @@ import android.content.IntentSender;
 
 
 public interface MessageCryptoCallback {
+    void onCryptoHelperProgress(int current, int max);
     void onCryptoOperationsFinished(MessageCryptoAnnotations annotations);
     void startPendingIntentForCryptoHelper(IntentSender si, int requestCode, Intent fillIntent,
             int flagsMask, int flagValues, int extraFlags) throws IntentSender.SendIntentException;
